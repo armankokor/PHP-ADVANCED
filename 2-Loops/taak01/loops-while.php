89b6786160c50e64b41493daf3d9f5c69fbb62c8
armankokor <2158154@talnet.nl>
2021-12-22 12:46:45 +0100

    opdracht 1

---
+++ 2-Loops/taak01/loops-while.php
@@ -0,0 +1,15 @@
+<?php
+ $leeftijd = 0 ;
+
+ while($leeftijd <= 17){
+   echo "Ik ben $leeftijd jaar, dus ik mag nog niet stemmen" . "<br>";
+   $leeftijd++;
+ }
+
+$leeftijd = 18 ;
+
+ while($leeftijd <= 18){
+   echo "Ik ben $leeftijd jaar, dus ik heb stemrecht!" . "<br>";
+   $leeftijd++;
+ }
+ ?>
