034530421bc06e2fc0286af7b9359969a9a52c1d
armankokor <2158154@talnet.nl>
2021-12-22 10:57:18 +0100

    opdracht 1

---
+++ 1-Arrays/taak01/index.php
@@ -0,0 +1,8 @@
+<?php
+$student['voornaam'] = "Arman";
+$student['achternaam'] = "Kokor";
+$student['klas'] = '1D';
+$student['leeftijd'] = 16;
+
+echo " Voornaam=" . $student['voornaam'] . " " . "Achternaam=" . $student['achternaam'] . " " . "Klas="  . $student['klas'] . " ". "Leeftijd=" . $student['leeftijd'];
+?>
