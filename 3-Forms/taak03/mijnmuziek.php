c07ef5043c07ae651158df7c88d440cc49c62f1d
armankokor <2158154@talnet.nl>
2022-01-12 16:08:26 +0100

    taak3

---
+++ 3-Forms/taak03/mijnmuziek.php
@@ -0,0 +1,25 @@
+*/http://localhost/php%202/PHP-ADVANCED/3-Forms/taak03/mijnmuziek2.php?artist=Buno+Mars&titel=24K+magic&year=2016&genre=pop*/
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+
+<form class= "Form" method="GET" action="mijnmuziek2.php">
+
+<p>Artiest <input name="Artist"></p>
+
+<p>Titel <input name="Titel"></p>
+
+<p>Jaar van uitgave <input name="Year"></p>
+
+<p>Genre <input name="Genre"></p>
+
+<button>Sturen</button>
+
+</form>
+</body>
---
+++ 3-Forms/taak03/mijnmuziek2.php
@@ -0,0 +1,5 @@
+<?php
+foreach($_GET as $A => $B){
+    echo $B . "<br>"; 
+}
+?>
